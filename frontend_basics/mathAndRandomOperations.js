83d6f8f2ab44d41ad87859bc8027b2b0ef00cd72
Dalton Bode <DBode@dev-10.com>
2024-05-16 18:50:35 -0500

    Adding randomness

--- frontend_basics/mathAndRandomOperations.js
+++ frontend_basics/mathAndRandomOperations.js
@@ -1,39 +1,58 @@
-const num = 2.489722458;
+const num = Math.random();
 const n = 1;
 const base = 10;
 const decimalPlace = base ** n;
 
 /**
  * Round numbers to the nth decimal place.
- * @param {Number} degree The decimal place you want to round to. Positive values are to the right of the decimal. Negative values are to the left of the decimal. Default is base 10 for now, but this can be altered if desired.
+ * @param {Number} num The number to round.
+ * @param {Int} degree The decimal place you want to round to. Positive values are to the right of the decimal. Negative values are to the left of the decimal. Default is base 10 for now, but this can be altered if desired.
  */
-const roundedNum = (degree) => {
+const roundedNum = (num, degree) => {
     round = Math.round(num * (degree)) / (degree);
     console.log(round);
     return round;
 };
-roundedNum(decimalPlace);
+roundedNum(num, decimalPlace);
 
 /**
  * Round down numbers to the nth decimal place.
- * @param {Number} degree The decimal place you want to round to. Positive values are to the right of the decimal. Negative values are to the left of the decimal. Default is base 10 for now, but this can be altered if desired.
+ * @param {Number} num The number to round.
+ * @param {Int} degree The decimal place you want to round to. Positive values are to the right of the decimal. Negative values are to the left of the decimal. Default is base 10 for now, but this can be altered if desired.
  */
-const flooredNum = (degree) => {
+const flooredNum = (num, degree) => {
     floor = Math.floor(num * (degree)) / (degree);
     console.log(floor);
     return floor;
 };
-flooredNum(decimalPlace);
+flooredNum(num, decimalPlace);
 
 /**
  * Round up numbers to the nth decimal place.
- * @param {Number} degree The decimal place you want to round to. Positive values are to the right of the decimal. Negative values are to the left of the decimal. Default is base 10 for now, but this can be altered if desired.
+ * @param {Number} num The number to round.
+ * @param {Int} degree The decimal place you want to round to. Positive values are to the right of the decimal. Negative values are to the left of the decimal. Default is base 10 for now, but this can be altered if desired.
  */
-const ceiledNum = (degree) => {
+const ceiledNum = (num, degree) => {
     ceil = Math.ceil(num * (degree)) / (degree);
     console.log(ceil);
     return ceil;
 };
-ceiledNum(decimalPlace);
+ceiledNum(num, decimalPlace);
 
+//Min, max and abs
+const arr = [1, 2, 5, 3, 2]
+
+const min = Math.min(...arr) // Output of 1 
+const max = Math.max(...arr) // Output of 5
+const abs = Math.abs(-5) // Output of 5
+
+console.log(`There are ${abs} elements in the array. The range for this array is ${min} to ${max}.`);
+
+const log2 = Math.log2(8); // 3
+const log10 = Math.log10(10); // 1
+
+console.log(log2);
+console.log(log10);
+
+// Use Infinity or -Infinity for comparisons or integrals. These values aren't actually infinitely large, just the largest storable number that can be stored in memory.
 
