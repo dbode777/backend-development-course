a2dbc6c3ba659a4067b28f1dcff5a5719368fcca
= <daltimusprime777@gmail.com>
2024-09-30 17:11:52 -0500

    NextJS Router functionality

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,6 +1,8 @@
 "use client"
 import { Post } from "@/components/Post";
 import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 // If a state change occurs in the Home function, it rerenders the whole page and every component in the page.
 function Text({ content, strong }) {
@@ -12,6 +14,8 @@ export default function Home() {
   const [author, setAuthor] = useState("");
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
+  const [userId, setUserId] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     console.log("Initializing Home")
@@ -31,14 +35,42 @@ export default function Home() {
     setContent("");
   }
 
+  function navigate() {
+    router.push(`/user/${userId}`);
+  }
+
   return (
     <div>
-      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title"/>
-      <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author"/>
-      <input type="text" value={content} onChange={(e) => setContent(e.target.value)} placeholder="Content"/>
+      <Link href="/about">About</Link>
+      <input 
+        type="text" 
+        value={title} 
+        onChange={(e) => setTitle(e.target.value)} 
+        placeholder="Title"
+      />
+      <input 
+        type="text" 
+        value={author} 
+        onChange={(e) => setAuthor(e.target.value)} 
+        placeholder="Author"
+      />
+      <input 
+        type="text" 
+        value={content} 
+        onChange={(e) => setContent(e.target.value)} 
+        placeholder="Content"
+      />
       <button onClick={addPost}>Add Post</button>
       <div className="posts"> 
         { posts.map((post, idx) => <Post key={idx + Math.random()} {...post} />) }
+      </div>
+      <div> 
+        <input type="text" 
+          placeholder="Search by user id" 
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+        />
+        <button onClick={navigate}>Go To Profile</button>
       </div>
     </div>
   )
